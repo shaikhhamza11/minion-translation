bace7db1e15cdda093bf628547611b98ce7c1b36
Hamza Shaikh <shaikh.hamza1199@gmail.com>
2021-09-12 20:52:52 +0530

    mark 5 done

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//url
+let url ="https://api.funtranslations.com/translate/minion.json";
+//btn
+let translatedBtn = document.querySelector("#btn");
+//textarea input
+let input = document.querySelector('#normalText')
+//textarea output
+let translatedOutput= document.querySelector('#translatedText')
+//click listener
+translatedBtn.addEventListener("click",()=>{
+    let text = input.value
+    let finalUrl = `${url}?text=${text}` 
+    fetch(finalUrl)
+    .then(response =>response.json() )
+    .then(json=>{
+console.log(json)
+        translatedOutput.innerText=json.contents.translated
+    })
+    .catch(err=>{
+        alert("Limit of request 5 reached")
+    })
+}) 
